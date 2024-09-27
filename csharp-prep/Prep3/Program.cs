bd67eef8e3392e5f693d371dbc9079631f9fcdbd
Logan-Lay21 <lay23004@byui.edu>
2024-09-26 20:49:29 -0600

    prep3 complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {   
+        string answer="yes";
+        do
+        {
+            Random randomGenerator = new Random();
+            int magicNum = randomGenerator.Next(1,100);
+            int guess=0;
+            int counter=0;
+
+            // Console.Write("What is the magic number? ");
+            // string magicString = Console.ReadLine();
+            // magicNum = int.Parse(magicString);
+
+            do 
+            {   
+                counter++;
+                Console.Write("What is your guess? ");
+                string guessString= Console.ReadLine();
+                guess = int.Parse(guessString);
+
+                if (guess > magicNum)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (guess < magicNum)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine($"You guessed it in {counter} guess(es)!");
+                }
+
+            } while (guess != magicNum);
+            Console.Write("Would you like to play again? ");
+            answer = Console.ReadLine();
+        } while (answer == "yes");
     }
 }
