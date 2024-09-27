07e916ee5ea0947ccc54e90545929249cdff2786
Logan-Lay21 <lay23004@byui.edu>
2024-09-27 17:38:27 -0600

    Code for C# Prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,37 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        DisplayResult(PromptUserName(),SquaredNumber(PromptUserNumber()));
     }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        Console.WriteLine(name);
+        return name;
+    }
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        string entry = Console.ReadLine();
+        int favNum = int.Parse(entry);
+        return favNum;
+    }
+    static int SquaredNumber(int num)
+    {
+        int square = num * num;
+        return square;
+    }
+    static void DisplayResult(string userName, int sqrNum)
+    {
+        Console.WriteLine($"{userName}, the square of your number is {sqrNum}");
+    }
+    
 }
