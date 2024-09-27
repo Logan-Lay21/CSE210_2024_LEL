143a6254b3c122a8cc3995a8517daa08b9995e36
Logan-Lay21 <lay23004@byui.edu>
2024-09-26 20:23:14 -0600

    Items from class

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
+    static void AwesomeFunction(int x)
+    {
+        Console.WriteLine($"The argumment {x} was passed to the awesome funciton");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        AwesomeFunction(65);
+        List<int> numberList;
+        numberList = new();
+
+        int input=-1;
+        while (input != 0) 
+        {
+            Console.Write("Enter a number: ");
+            string inputString = Console.ReadLine();
+            input = int.Parse(inputString);
+            if (input != 0)
+            {
+                numberList.Add(input);
+            }
+        }
+
+        foreach (int item in numberList)
+        {
+            Console.WriteLine(item);
+        }
+
     }
 }
