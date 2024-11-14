27e888426d2afd3c221509cc332fddbfb299611a
Logan-Lay21 <lay23004@byui.edu>
2024-11-13 18:23:23 -0700

    Journal Project

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -6,44 +6,19 @@ using System.Security.Cryptography.X509Certificates;
 class Journal
 {
     public List<Entry> _entries = new();
-
-    
-    public string filename = "";
-
-    public void WriteFile() //Save Function
+    public void WriteFile(string filename) //Save Function
     {
-        Console.Write("Enter a filename: ");
-        filename = Console.ReadLine();
         using (StreamWriter outputFile = new StreamWriter(filename))
         {
             foreach (Entry x in _entries)
             {
                 x.WriteEntry(outputFile);
             }
-            // outputFile.WriteLine("This will be the firstline in the file");
-
-            // string color = "Blue";
-            // outputFile.WriteLine($"My favorite color is {color}");
         }
 
     }
     public void DisplayJournal()
     {
-        string[] lines = File.ReadAllLines(filename);
-
-        foreach (string line in lines)
-        {
-            string[] parts = line.Split("~");
-
-            Entry newEntry = new();
-            newEntry._date = parts[0];
-            newEntry._name = parts[1];
-            newEntry._prompt = parts[2];
-            newEntry._entry = parts[3];
-
-
-        }
-
         foreach (Entry x in _entries)
         {
             x.DisplayEntry();
@@ -57,15 +32,22 @@ class Journal
 
         _entries.Add(newEntry);
     }
+
+    public void LoadJournal(string filename)
+    {
+        string[] lines = System.IO.File.ReadAllLines(filename);
+
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split("~");
+
+            Entry newEntry = new();
+            newEntry._date = parts[0];
+            newEntry._name = parts[1];
+            newEntry._prompt = parts[2];
+            newEntry._entry = parts[3];
+
+            _entries.Add(newEntry);
+        }
+    }
 }
-
-
-// string[] lines = System.IO.File.ReadAllLines(filename);
-
-// foreach (string line in lines)
-// {
-//     string[] parts = line.Split(",");
-
-//     string firstName = parts[0];
-//     string lastName = parts[1];
-// }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -5,53 +5,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Journal journal1 = new();
-        // journal1.InputNewEntry();
-        // journal1.InputNewEntry();
-        // journal1.WriteFile();
-        // journal1.DisplayJournal();
-        // Entry test = new();
-        // test.InputEntry();
-        // test.DisplayEntry();
-        int choice = 0;
+        Journal j = new();
+
+        string choice = "";
         do 
         {
             Console.WriteLine("1. Write\n2. Display\n3. Load\n4. Save\n5. Quit");
 
             Console.Write("What would you like to do? ");
-            choice = int(Console.ReadLine());
+            choice = Console.ReadLine();
             
-            if (choice == 1) {
-
+            if (choice == "1") { //Write
+                j.InputNewEntry();
             }
 
-            else if (choice == 2) {
-
+            else if (choice == "2") { //Display
+                j.DisplayJournal();
             }
 
-            else if (choice == 3) {
-                
+            else if (choice == "3") { //Load
+                Console.Write("Enter a filename: ");
+                string filename = Console.ReadLine();
+                j.LoadJournal(filename);
             }
 
-            else if (choice == 4) {
-                
+            else if (choice == "4") { //Save
+                Console.Write("Enter a filename: ");
+                string filename = Console.ReadLine();
+                j.WriteFile(filename);
             }
 
-        } while (choice != 5);
+        } while (choice != "5"); //Quit
     }
 
 }
-// string filename = "myFile.txt";
-// string[] lines = System.IO.File.ReadAllLines(filename);
-
-// foreach (string line in lines)
-// {
-//     Console.WriteLine(line);
-// }
-/* 
-1. Write
-2. Display
-3. Load
-4. Save
-5. Quit
-*/
